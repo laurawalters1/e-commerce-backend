806de3c12953489f79b522b312a86d1f10e68331
Laura Walters <laura.walters1098@yahoo.com>
2022-03-21 20:59:20 +0000

    Setting up table structures

--- models/index.js
+++ models/index.js
@@ -1,16 +1,31 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
+
+// You'll need to execute association methods on your Sequelize models to create the following relationships between them:
+
+// Product belongs to Category, and Category has many Product models, as a category can have multiple products but a product can only belong to one category.
+
+// Product belongs to many Tag models, and Tag belongs to many Product models. Allow products to have multiple tags and tags to have many products by using the ProductTag through model.
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag });
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
